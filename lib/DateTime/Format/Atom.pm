98e31b797ca22b3e4add64f77680c08cabd293bc
Eric Brine <ikegami@adaelis.com>
2025-01-05 08:30:11 -0400

    v1.8.0

--- lib/DateTime/Format/Atom.pm
+++ lib/DateTime/Format/Atom.pm
@@ -4,7 +4,7 @@ package DateTime::Format::Atom;
 use strict;
 use warnings;
 
-use version; our $VERSION = qv( 'v1.7.0' );
+use version; our $VERSION = qv( 'v1.8.0' );
 
 use DateTime::Format::RFC3339 qw( );
 
@@ -65,7 +65,7 @@ DateTime::Format::Atom - Parse and format Atom datetime strings
 
 =head1 VERSION
 
-Version 1.7.0
+Version 1.8.0
 
 
 =head1 SYNOPSIS
